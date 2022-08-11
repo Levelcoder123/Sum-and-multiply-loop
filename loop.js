18ed9918b7c296f1d3bed13e0309ae40d6d2ea32
Levelcoder123 <108260254+Levelcoder123@users.noreply.github.com>
2022-08-11 12:21:37 +0500

    Sum and multiply loop

---
+++ loop.js
@@ -0,0 +1,20 @@
+function loopStart(){
+let sumMake=document.getElementById('sum').value;
+let samin=0;
+
+for (let x = 1; x <=sumMake ; x++){
+
+samin +=x ;}
+
+document.getElementById('sam').innerHTML=samin;
+}
+
+function oceanTide(){
+let sumMake=document.getElementById('sim').value;
+
+let sami=0;
+
+for (let x = 1; x <=sumMake ; x++){
+sami=x*1.5;}
+document.getElementById('som').innerHTML=sami;
+    }
